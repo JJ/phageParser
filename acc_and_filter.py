63b17618d285a53a8807bfe7efda0c7a93a0b336
JJ Merelo <jjmerelo@gmail.com>
2015-06-12 20:58:47 +0200

    Filtering while searching
    This refers to goyalsid/phageParser#47
    
    I have added a new script, in order to leave the previous one as it is. In order to make debugging easier, it takes two parameters, email and accession number this way:
    
    acc_and_filter.py youre@ma.il data/bac_accession_list_tiny.txt
    
    This second file includes one accession number with Cas3 and others. And for the time being it just prints the product in case it finds Cas3 and others. I don't know Python, have worked only occassionally with `Bio` (and usually in Perl and FASTA) and this was just a proof of concept.
    
    As you see, I use Bio.Seq. It's probably a more sensible choice to do it just the way you did it before: producing gb files and then dealing with them; but you can just as well work on the humongous GB file using Bio.SeqIO (in Python) or Bio::SeqIO (in Perl). No need at all to use regular expressions. My point is that by looking at how you run over the records it should be easy to rewrite either cas_in_gb (using dependencies, no other way) or do it in Python reading from file instead of using `efetch`
    
    At any rate, check it now to see if it's close to what you're looking for and we can take it from there, making output resemble what we had before with the Perl script or rewriting the Perl script (in Perl or Python) so that it acts on the humongous example file.
    
    Don't pull request now, but you can pull it form my copy of the repository.

--- acc_and_filter.py
+++ acc_and_filter.py
@@ -6,6 +6,12 @@ Adapted from https://www.biostars.org/p/66921/
 USAGE:
 cat <file> | python acc2gb.py <email> > <output>
 
+or
+
+python acc2gb.py <email> <file>
+
+(this is easier for debugging)
+
 where:
 <file> is the name of a file containing accession numbers to download
 <email> is the email address associated with your NCBI account
@@ -16,25 +22,41 @@ Biopython
 """
 
 import sys
-from Bio import Entrez
+from Bio import Entrez,SeqIO
+import pprint
+import re
 
 #define email for entrez login
 db           = "nuccore"
 Entrez.email = sys.argv[1]
 
 #get accession numbers out of stdin
-if ( sys.stdin ) 
-    accs = [ l.strip() for l in sys.stdin if l.strip() ]
-else
+if sys.argv[2] :
     accs = [line.rstrip('\n') for line in open(sys.argv[2])]
+else :
+    accs = [ l.strip() for l in sys.stdin if l.strip() ]
+
 
 #fetch
 sys.stderr.write( "Fetching %s entries from GenBank: %s\n" % (len(accs), ", ".join(accs[:10])))
+                  
+pp = pprint.PrettyPrinter(indent=4)
+
 for i,acc in enumerate(accs):
   try:
     sys.stderr.write( " %9i %s          \r" % (i+1,acc))  
     handle = Entrez.efetch(db=db, rettype="gbwithparts", id=acc)
-    #print output to stdout
-    sys.stdout.write(handle.read())
+    for record in SeqIO.parse(handle,"gb"):
+        print "Name %s, %i features" % (record.name, len(record.features))
+        for f in record.features:
+            if f.type == 'CDS' and f.qualifiers['product']:
+                    cas = re.search("(Cas3|Cas10|Cas9)", f.qualifiers['product'][0] )
+                    if cas and cas.group(0): 
+                        print "Location %s Product %s" % (f.location, f.qualifiers['product'])
+            
+    handle.close()
+
   except:
     sys.stderr.write( "Error! Cannot fetch: %s        \n" % acc) 
+
+    
